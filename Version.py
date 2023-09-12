a5d2ac4d317a0606e4028d285e949f299b1a53d8
TeaCookie <xinheng@ualberta.ca>
2023-09-11 18:25:20 -0600

    Update Version.py

--- Version.py
+++ Version.py
@@ -2,4 +2,6 @@ import requests
 
 print(requests.__version__)
 
-# print(requests.get("")).text
+res = requests.get("https://raw.githubusercontent.com/TeaCookie/404lab1/main/Version.py")
+
+print(res.text)
